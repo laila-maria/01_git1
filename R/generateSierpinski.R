5d882e6ae5aff9d8dfdbd1291600e8b307d80261
laila-maria <laila.rothe@campus.lmu.de>
2024-10-15 14:26:47 +0200

    .

--- R/generateSierpinski.R
+++ R/generateSierpinski.R
@@ -3,3 +3,9 @@ generateSierpinski <- function(rows) {
   # stub function that generates random rows
   data.frame(x=runif(rows), y=runif(rows))
 }
+
+1+1
+library(git)
+install.packages("gitcreds")
+library(gitcreds)
+gitcreds::gitcreds_set()
